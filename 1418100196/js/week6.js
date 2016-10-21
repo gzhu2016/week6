6d43a8f52a6ea0cee5831b1a64da4791c7f576f8
yingfff <761154670@qq.com>
2016-10-21 19:41:13 +0800

    create 1418100196 魏莹的第四次作业

---
+++ 1418100196/js/week6.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Administrator on 2016/10/20.
+ */
+//第一题
+function sum(n){
+    var sum=0;
+    for(var i=1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+
+}
+//第二题
+function sum2(n){
+    var sum=0;
+    for(var i=0;i<=n;i+2){
+        sum+=i;
+    }
+    return sum2;
+}
+//第三题
+function rectangle(height,width){
+    var data=new Array();
+    data[0]=2*(height*width);
+    date[1]=height*width;
+    return data;
+}
